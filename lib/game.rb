c195b00ad51119be614822325d951b3fcb66229e
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-14 20:41:28 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -10,9 +10,14 @@ class Game
     [0,4,8],
     [2,4,6]
   ]
+  
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+  end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? @player_2 : @player_1
   end
 end
