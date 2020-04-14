b17abee6be044a04ac6a80a41a8cabb08db891bb
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-14 22:43:02 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,6 +9,19 @@ module Players
         move = "1"
       elsif board.turn_count == 2
         move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move = 2
+      else
+        Game::WIN_COMBINATIONS.detect do |c|
+          if c.select{|i| board.position(i+1) == token}.size == 2 && c.any?{|i| board.position(i+1) == " "}
+            move = c.select{|i| !board.taken(i+1)}.first.to_i + 1
+          elsif c.select{|i| board.position(i+1) != token && board.position{i+1} != " "}.size == 2 && c.any?{|i| board.position(i+1) == " "}
+            move = c.select{|i| !board.talem(i+1)}.first.to_i + 1
+          end
+        end
+        move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken(i)} if move == nil
+      end
+      move  
     end
   end
 end
